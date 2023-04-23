0b884dd12a1ee47edc5aee89d2ca10adc5f90e4b
Svetlanaaaa996 <61981638+Svetlanaaaa996@users.noreply.github.com>
2023-04-23 22:16:50 +0200

    Multiple paralell

--- phptravelsAutomation/src/main/java/Demo/TestNGMultipleBrowserDemo.java
+++ phptravelsAutomation/src/main/java/Demo/TestNGMultipleBrowserDemo.java
@@ -8,13 +8,14 @@ import driver.WebDriverExecuter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.io.IOException;
-
+import Demo.TestDataReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -27,6 +28,13 @@ public class TestNGMultipleBrowserDemo {
 	private static final String homeUrl = "https://phptravels.net/";
 	LoginPage loginPage;
 	WebDriverExecuter webDriverExecuter;
+	  TestDataReader testDataReader;
+	  @BeforeClass
+	    public void prepareTestDataReader() throws IOException {
+	        // 0 index is login credentials sheet.
+	        this.testDataReader = new TestDataReader(0);
+	    }
+	
 	
 	@Parameters("browserName")
 	@BeforeTest
@@ -52,14 +60,56 @@ public class TestNGMultipleBrowserDemo {
 
 	        loginPage = homePage.getLoginPage();
 	    }
-	@Test
-	public void pub() {
-	        loginPage.setLoginCredentials("Svetlana@test", "");
-	        System.out.println(loginPage.getPasswordFieldErrorMessage());
+	  @Test(priority = 1, description = "Validate email and password blank fields")
+	    public void validateEmailAndPasswordBlankFields() {
+	        loginPage.setLoginCredentials(testDataReader.getData(1, 0),
+	                                      testDataReader.getData(1, 1));
+	        Assert.assertEquals(loginPage.getEmailFieldErrorMessage(),
+	                            "Please fill out this field.");
 	    }
-	
-	
-	 @AfterMethod
+
+	    @Test(priority = 2, description = "Validate when password is a blank field")
+	    public void validatePasswordBlankField() {
+	        loginPage.setLoginCredentials(testDataReader.getData(2, 0),
+	                                      testDataReader.getData(2, 1));
+	        Assert.assertEquals(loginPage.getPasswordFieldErrorMessage(),
+	                            "Please fill out this field.");
+	    }
+
+	    @Test(priority = 3, description = "Validate email when it doesn't have @")
+	    public void validateEmailFieldWithoutAtSign() {
+	        loginPage.setLoginCredentials(testDataReader.getData(3, 0),
+	                                      testDataReader.getData(3, 1));
+	        Assert.assertEquals(loginPage.getEmailFieldErrorMessage(),
+	                            "Please include an '@' in the email address."
+	                            + " 'TestwithoutAtsign' is missing an '@'.");
+	    }
+
+	    @Test(priority = 4, description = "Validate email when it doesn't have domain name")
+	    public void validateEmailFieldWithoutDoaminName() {
+	        loginPage.setLoginCredentials(testDataReader.getData(4, 0),
+	                                      testDataReader.getData(4, 1));
+	        Assert.assertEquals(loginPage.getEmailFieldErrorMessage(),
+	                "'.' is used at a wrong position in '.com'.");
+	    }
+
+	    @Test(priority = 5, description = "Validate email when it doesn't have user name")
+	    public void validateEmailFieldWithoutUserName() {
+	        loginPage.setLoginCredentials(testDataReader.getData(5, 0),
+	                                      testDataReader.getData(5, 1));
+	        Assert.assertEquals(loginPage.getEmailFieldErrorMessage(),
+	                            "Please enter a part followed by '@'. '@gmail.com'"
+	                            + " is incomplete.");
+	    }
+
+	    @Test(priority = 6, description = "Validate invalid username and password")
+	    public void validateInvalidEmailAndPassword() {
+	        loginPage.setLoginCredentials(testDataReader.getData(6, 0),
+	                                      testDataReader.getData(6, 1));
+	        Assert.assertTrue(loginPage.isErrorMessageDisplayed());
+	    }
+
+	    @AfterMethod
 	    public void closeDriver() {
 	        webDriverExecuter.closeBrowser();
 	    }
---
+++ phptravelsAutomation/src/test/java/login/RegisterTests.java
@@ -0,0 +1,10 @@
+package login;
+
+import java.util.UUID;
+
+public class RegisterTests {
+	// Generate a random email address
+    String randomText = UUID.randomUUID().toString();
+    String email = "test+" + randomText + "@gmail.com";
+	
+}
